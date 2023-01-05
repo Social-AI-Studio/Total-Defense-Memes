9a67a210142321fb336d6d11d6280009df603e60
mingshanhee <mingshan.hee@gmail.com>
2023-01-05 15:59:42 +0800

    remove redundant functions

--- annotation-platform/backend/controllers/meme.controller.js
+++ annotation-platform/backend/controllers/meme.controller.js
@@ -13,69 +13,6 @@ let NoNewMemesError = () => {
   return err;
 };
 
-const upload = async (req, res) => {
-  try {
-    if (req.file == undefined) {
-      return res.status(400).send("Please upload a CSV file!");
-    }
-
-    let batches = new Set();
-    let memes = [];
-    let path = __basedir + "/uploads/" + req.file.filename;
-
-    fs.createReadStream(path)
-      .pipe(csv.parse({ headers: true }))
-      .on("error", (error) => {
-        throw error.message;
-      })
-      .on("data", (row) => {
-        batches.add(row['batch'])
-        memes.push(row)
-      })
-      .on("end", () => {
-
-        // Create batches
-        var batchObjects = []
-        batches.forEach(x => {
-          batchObjects.push({
-            "name": x
-          })
-        });
-
-        Batch.bulkCreate(batchObjects).then((batches) => {
-
-          // Remap batches
-          var batch2Id = {}
-          batches.forEach(element => {
-            batch2Id[element.name] = element.id
-          });
-
-          // Update meme objects
-          for (let i = 0; i < memes.length; i++) {
-            const element = memes[i];
-            element['batchId'] = batch2Id[element['batch']]
-          }
-
-          return Meme.bulkCreate(memes)
-        }).then((memes) => {
-          res.status(200).send({
-            message:
-              "Uploaded the file successfully: " + req.file.originalname,
-          });
-        }).catch((err) => {
-          console.log(err)
-          res.status(500).send({
-            message: err,
-          });
-        })
-      });
-  } catch (error) {
-    res.status(500).send({
-      message: "Could not upload the file: " + req.file.originalname,
-    });
-  }
-};
-
 const addMemesToBatch = async (req, res) => {
   try {
     if (req.file == undefined) {
@@ -193,21 +130,6 @@ const addMemesToBatch = async (req, res) => {
   }
 };
 
-const getMemes = (req, res) => {
-  Batch.findAll()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving tutorials.",
-      });
-    });
-};
-
 module.exports = {
-  upload,
   addMemesToBatch,
-  getMemes
 };
